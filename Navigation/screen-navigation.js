9721d93c5eeeb903454bb82cc4c7346566a79b08
Bilal1509 <bilalsd15@gmail.com>
2025-01-14 14:49:06 +0500

    First

---
+++ Navigation/screen-navigation.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import LoginScreen from '../screens/LoginScreen';
+// import RegisterScreen from '../screens/RegisterScreen';
+
+import Profile  from '../src/Profile';
+
+
+const Stack = createNativeStackNavigator();
+
+const ScreenNavigator = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
+           
+        </Stack.Navigator>
+    )
+}
+export default ScreenNavigator
